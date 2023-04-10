36512265a208a949e89f4c60e94d62419519f9cd
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-04-11 01:17:08 +0700

    modify fragment login and edit item layout

--- app/src/main/java/com/example/movie_kelompok3/Fragment/LoginFragment.kt
+++ app/src/main/java/com/example/movie_kelompok3/Fragment/LoginFragment.kt
@@ -5,56 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.movie_kelompok3.R
+import com.example.movie_kelompok3.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var listAccount: MutableMap<String, String> = mutableMapOf()
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listAccount["account1"] = "password1"
+        listAccount["account2"] = "password2"
+
+        binding.btnSignIn.setOnClickListener {
+            checkAccount()
+        }
+
+    }
+
+    fun checkAccount(){
+        val inputUsername = binding.inputUsername.text.toString()
+        val inputPassword = binding.inputPassword.text.toString()
+        if(inputUsername.isNotEmpty() && inputPassword.isNotEmpty()){
+            var accountFailed = true
+            for (account in listAccount){
+                if(account.key == inputUsername && account.value == inputPassword){
+                    val bundleData = Bundle().apply {
+                        putString("USERNAME", inputUsername)
+                    }
+                    accountFailed = false
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment, bundleData)
+                    break
                 }
             }
+            if(accountFailed){
+                Toast.makeText(context, "username & password yang anda masukan salah!", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(context, "username & password tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
